e8b27ba39b2bda34f663e6806ad2bb9ba189bdcc
Imandeep Kaur <imanpunia19@gmail.com>
2021-10-02 21:53:46 +1000

    JL: complaint to m*n matrix
    Before it was for 2*2 ,but now it will work for m*n

--- Matrix/BeforeMatrix.java
+++ Matrix/BeforeMatrix.java
@@ -5,27 +5,26 @@ public class BeforeMatrix {
         public static void beforeMatrixValue(int arr[][]){
 
             int after[][] = arr;
-            int m = after.length;
-            int n = after[0].length;
+            int m = after.length; //rows
+            int n = after[0].length; //columns
             int before[][] = new int[m][n];
 
-            //keep the sum of common repetitive matrix element
-            int sum = 0;
+            //removed sum  because in case of m and n > 2 ,the summation is not calculated correct when you tries to fill a11 element
+
 
             for(int i = 0; i < m; i++){
-                int beforeRow[] = new int[2];
+                int beforeRow[] = new int[m];
                 for(int j = 0; j < n ; j++){
-                    //when its the first element - a00
-                    if(i < 1&& j < 1){
+                    //when its the first element -> a00 -> i = 0 , j =  0
+                    if(i < 1 && j < 1){
                         beforeRow[j] = arr[i][j];
-                        sum = sum +  arr[i][j];
-                    }  else if(i > 0 && j > 0){
-                    //when its the last element  - a11
-                        beforeRow[j] = arr[i][j]  - sum ;
-                    } else {
+                    }  else if(i >= 1 && j >= 1) {
+                        beforeRow[j] = arr[i][j] - arr[i][j-1] - arr[i-1][j] + arr[i-1][j-1];
+                    } else if( i == 0 && j > 0){
                     //when its the top riight and bottom left element - a01 && a10
-                        beforeRow[j] = arr[i][j] - arr[0][0];
-                        sum = sum + arr[i][j] - arr[0][0];
+                        beforeRow[j] = arr[i][j] - arr[i][j-1];                      
+                    } else if(i > 0 && j == 0){
+                        beforeRow[j] = arr[i][j] - arr[i-1][j];
                     }
                 }
                 before[i] = beforeRow; 
@@ -41,10 +40,25 @@ public class BeforeMatrix {
         }
 
         public static void main(String args[]){
-            int arr1[][] = {{2,5},
+            int arr1[][] = {{10,30,60},
+                            {20,60,120},
+                            {60,110,180}};
+
+            int arr2[][] = {{2,5},
                             {7,17}};
             
+            beforeMatrixValue(arr2);
             beforeMatrixValue(arr1);
         }
 
 }
+
+/**
+   output -        
+            2       3
+            5       7
+
+            10      20      30
+            10      20      30
+            40      10      10
+ */
